d71a21fa8c504a59acfc9b139eddf6b33251d986
Christoph Giesel <mail@cgiesel.de>
2012-09-08 22:29:03 +0200

    erster commit mit stammtisch node template

---
+++ README.md
@@ -0,0 +1,27 @@
+# Piratenkleider LSA
+Drupal-Theme für Webauftritte der Piratenpartei Sachsen-Anhalt
+
+## Lizenz
+[CC-BY-SA 3.0](http://creativecommons.org/licenses/by-sa/3.0/de/deed.de)
+
+## Allgemeines
+Dies ist ein Subtheme von Piratenkleider für die Webseite der Piratenpartei
+Sachsen-Anhalt.
+
+Hier sollen Änderungen hinzugefügt werden, die nur für die LSA Webseite
+sein sollen.
+
+## Installation
+Einfach per Git das Repository "clonen" bzw. herunterladen. Den Ordner in
+"piratenkleiderlsa" umbenennen und nach "/sites/all/themes/" verschieben.
+
+Außerdem wird das Basis Theme "Piratenkleider" benötigt:
+https://github.com/PiratenLSA/drupal-piratenkleider
+
+## Features
+
+### Stammtische
+Es wurde ein Stamtisch Node angelegt. Dieses Theme enthält ein Template und
+CSS-Definitionen für diese Nodes.
+
+Ein Feature-Module wird hierzu noch veröffentlicht.
---
+++ node--stammtisch.tpl.php
@@ -0,0 +1,63 @@
+<div id="node-<?php print $node->nid; ?>" class="post <?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <div class="post-title">
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    </div>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <div class="post-entry"<?php print $content_attributes; ?>>
+    <?php
+      print render($content['field_bilder']);
+    ?>
+
+    <?php
+      print render($content['field_ort']);
+    ?>
+
+    <br />
+
+    <?php
+      print render($content['field_turnus']);
+    ?>
+
+    <br />
+
+    <?php
+      print render($content['field_datum']);
+    ?>
+
+    <br />
+
+    <?php
+      print render($content['field_wiki_url']);
+    ?>
+
+    <br />
+
+    <div style="clear: both;"></div>
+
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_ort_url']);
+      print render($content);
+    ?>
+  </div>
+
+  <div class="post-meta">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+  </div>
+
+  <hr />
+
+  <div class="post-comments" id="comments">
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
